260a8b8e2478a0028eccbd0fede3678c5e3d7a2c
DucAnh <ducanh2k2@gmail.com>
2022-08-08 21:33:23 +0700

    ReactJS

--- src/components/Main/Shop/shop.js
+++ src/components/Main/Shop/shop.js
@@ -74,6 +74,64 @@ import { ListItems } from "../../../App";
 //     }
 // ];
 
+function HeaderProduct({firstArrayItem,setArrayItem}) {
+    function handleSort(e) {
+        const type = e.target.value
+        switch(type) {
+            case "Relevance": 
+                setArrayItem(() => {
+                    return [...firstArrayItem]
+                })
+                break;
+            case "Name: A-Z":
+                setArrayItem(preArrayItem => {
+                    const arrayItem = [...preArrayItem]
+                    arrayItem.sort((a,b) => a.product_name.localeCompare(b.product_name))
+                    return arrayItem
+                })
+                break;
+            case "Name: Z-A":
+                setArrayItem(preArrayItem => {
+                    const arrayItem = [...preArrayItem]
+                    arrayItem.sort((a,b) => b.product_name.localeCompare(a.product_name))
+                    return arrayItem
+                })
+                break;
+            case "Price: Low to High":
+                setArrayItem(preArrayItem => {
+                    const arrayItem = [...preArrayItem]
+                    arrayItem.sort((a,b) => a.price - b.price)
+                    return arrayItem
+                })
+                break;
+            case "Price: High to Low":
+                setArrayItem(preArrayItem => {
+                    const arrayItem = [...preArrayItem]
+                    arrayItem.sort((a,b) => b.price - a.price)
+                    return arrayItem
+                })
+                break;
+            default:
+                throw new Error("Sort Error!")
+        }
+    }
+    return (
+        <div className="header__product-wrap">
+            <div className="sort__product-wrap">
+                <label className = "label-sort-product" htmlFor = "sort-product">Sort by:</label>
+                <select id = "sort-product" name = "sort-product" onChange={handleSort}>
+                    <option className="option-sort-product">Relevance</option>
+                    <option className="option-sort-product">Name: A-Z</option>
+                    <option className="option-sort-product">Name: Z-A</option>
+                    <option className="option-sort-product">Price: Low to High</option>
+                    <option className="option-sort-product">Price: High to Low</option>
+                </select>
+            </div>
+        </div>
+    )
+}
+
+let firstArrayItem = []
 export default function Shop() {
     // toast_messges    
     const notify = () => toast.success('Add to cart successfuly!', { // toast_messages
@@ -85,6 +143,7 @@ export default function Shop() {
         draggable: true,
         progress: undefined,
     });
+
     const [arrayItem,setArrayItem] = useState([]) // use get product from API
     const [listItems,setListItems] = useContext(ListItems) // use save item when click add
     const [itemPerPage,setItemPerPage] = useState(12) // use to count the number of products per page 
@@ -101,6 +160,7 @@ export default function Shop() {
                 e.images = "http://petsla-api.herokuapp.com" + e.images
             });
         console.log("Finish load API!")
+            firstArrayItem = data
             setArrayItem(data)
         })
         // 40 items
@@ -133,51 +193,53 @@ export default function Shop() {
     }
     return (
     <div className="shop grid wide">
-    <div className="content__list-items">
-        <div className="toast-messages">
-            <ToastContainer/>
-        </div>
-        <div className="list-items__wrap">
-            {
-                arrayItem.map((item,idx) => {
-                    if(idx < itemPerPage*currentPage && idx >= itemPerPage*(currentPage-1)) {
-                        return (
-                            <div className="col c-6 m-4 l-3 item" key = {idx} id = {item.id}>
-                                <a className="item__product" href="/">
-                                    <div className="item__image" style={{backgroundImage:`url(${item.images})`}}/>
-                                </a>
-                                <div className="item__content">
-                                    <div className="item__desc">
-                                        <a href="/">
-                                            <div className="item__desc-title">
-                                                {item.product_name}
+        <HeaderProduct firstArrayItem = {firstArrayItem} setArrayItem = {setArrayItem}/>
+        <div className="content__list-items">
+            <div className="toast-messages">
+                <ToastContainer/>
+            </div>
+            <div className="list-items__wrap">
+                {
+                    arrayItem.map((item,idx) => {
+                        if(idx < itemPerPage*currentPage && idx >= itemPerPage*(currentPage-1)) {
+                            return (
+                                <div className="col c-6 m-4 l-3 item" key = {idx} id = {item.id}>
+                                    <a className="item__product" href="/">
+                                        <div className="item__image" style={{backgroundImage:`url(${item.images})`}}/>
+                                    </a>
+                                    <div className="item__content">
+                                        <div className="item__desc">
+                                            <a href="/">
+                                                <div className="item__desc-title">
+                                                    {item.product_name}
+                                                </div>
+                                            </a>
+                                            <div className="item__desc-price">
+                                                {item.price.toLocaleString('vi')}
                                             </div>
-                                        </a>
-                                        <div className="item__desc-price">
-                                            {item.price.toLocaleString('vi')}
                                         </div>
-                                    </div>
-                                    <div className="item__buy-cart-wrap">
-                                        <div className="item__buy">
-                                            <i className="item__buy-icon fa-solid fa-bag-shopping"></i>
-                                            <span>Buy now</span>
-                                        </div>
-                                        <div className="item__cart" onClick={addItem}>
-                                            <i className="item__cart-icon fa-solid fa-cart-plus"></i>
-                                            <span>Add to Cart</span>
+                                        <div className="item__buy-cart-wrap">
+                                            <div className="item__buy">
+                                                <i className="item__buy-icon fa-solid fa-bag-shopping"></i>
+                                                <span>Buy now</span>
+                                            </div>
+                                            <div className="item__cart" onClick={addItem}>
+                                                <i className="item__cart-icon fa-solid fa-cart-plus"></i>
+                                                <span>Add to Cart</span>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    }
-                })
-            }
+                            )
+                        }
+                    })
+                }
+            </div>
         </div>
-    </div>
         <div className="pagination">
             <ul className="pagination-wrap">
-                <li className={`page-item ${(currentPage == 1)?("page-item--disable"):("")}`} style={{borderTopLeftRadius: ".2rem", borderBottomLeftRadius: ".2rem"}}>
+                <li className={`page-item ${(currentPage == 1)?("page-item--disable"):("")}`} style={{borderTopLeftRadius: ".2rem", borderBottomLeftRadius: ".2rem"}}
+                    onClick = {() => {if( currentPage > 1) { setCurrentPage(preCurrentPage => preCurrentPage-1)}}}>
                     <i className="page-link fa-solid fa-angle-left"></i>
                 </li>
                 {
@@ -197,7 +259,8 @@ export default function Shop() {
                         return pageItems
                     }()
                 }
-                <li className={`page-item ${(currentPage == numberPages)?("page-item--disable"):("")}`} style={{borderTopRightRadius: ".2rem", borderBottomRightRadius: ".2rem"}}>
+                <li className={`page-item ${(currentPage == numberPages)?("page-item--disable"):("")}`} style={{borderTopRightRadius: ".2rem", borderBottomRightRadius: ".2rem"}}
+                    onClick = {() => {if( currentPage < numberPages) { setCurrentPage(preCurrentPage => preCurrentPage+1)}}}>
                     <i className="page-link fa-solid fa-angle-right"></i>
                 </li>
             </ul>
--- src/index.js
+++ src/index.js
@@ -5,3 +5,17 @@ import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App/>)
+
+
+/*
+2) display xám
+*/
+
+
+/* Done: Sử dụng framework ReactJS
+Fetch API product
+Lựa chọn số items/page và select page
+Sắp xếp sản phẩm
+Menu cart: thêm, xóa, tăng, giảm sản phẩm vào gian hàng
+Responsive trên pc, latop, mobile
+*/
