12f7204da8974e8379ae65d49bd06446083b020a
DucAnh <ducanh2k2@gmail.com>
2022-08-08 23:30:24 +0700

    ReactJS

--- src/index.js
+++ src/index.js
@@ -5,13 +5,3 @@ import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App/>)
-
-
-/* 
-Done: Sử dụng framework ReactJS và JQuery (scrollTop)
-    Fetch API product
-    Lựa chọn số items/page và select page
-    Sắp xếp sản phẩm
-    Menu cart: thêm, xóa, tăng, giảm sản phẩm vào gian hàng
-    Responsive trên pc, latop, mobile
-*/
